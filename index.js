73314efa781fcb106166ea0829455efb498dc6e8
rockbot <raquel@rckbt.me>
2015-05-07 14:21:26 -0700

    adds options object for configuring ipHeader

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var strftime = require('prettydate').strftime;
 
-module.exports = function generateCommonLog(request, response)
+module.exports = function generateCommonLog(request, response, options)
 {
     var protocol = 'HTTP/' + request.httpVersion;
     var payload_len = response._data ? response._data.length : '-';
     var UA = request.headers['user-agent'] || '-';
     var referer = request.headers['referer'] || '-';
     var tstamp = response._time ? new Date(response._time) : new Date();
-    var remote = (request.socket ? request.socket.remoteAddress : '') || '';
+    var remote = (options && options.ipHeader ? request.headers[options.ipHeader] : '') ||
+                 (request.socket ? request.socket.remoteAddress : '') || '';
     var accepts = request.headers['accept'] || '-';
     var elapsed = response._time ? (Date.now() - response._time) + ' ms' : '';
 
